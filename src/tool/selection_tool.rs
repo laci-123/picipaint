4344f1ef4b153dc940b580ef47b4feaf53411e0c
Gohér László <goher.laci@gmail.com>
2025-02-16 13:43:37 +0100

    multiple objects can be selected by holding down SHIFT

--- src/tool/selection_tool.rs
+++ src/tool/selection_tool.rs
@@ -11,9 +11,25 @@ impl SelectionTool {
 
 impl Tool for SelectionTool {
     fn update(&mut self, response: &egui::Response, objects: &mut Vec<Box<dyn PaintObject>>) {
-        if let Some(mouse_pos) = response.interact_pointer_pos() {
-            for object in objects.iter_mut() {
-                object.set_selected(object.is_under_mouse(mouse_pos.to_vec2()));
+        let is_shift_down =
+        response.ctx.input(|input| {
+            input.modifiers.matches_exact(egui::Modifiers::SHIFT)
+        });
+
+        if response.clicked_by(egui::PointerButton::Primary) {
+            if let Some(mouse_pos) = response.interact_pointer_pos() {
+                for object in objects.iter_mut() {
+                    let is_under_mouse = object.is_under_mouse(mouse_pos.to_vec2());
+
+                    if is_shift_down {
+                        if is_under_mouse {
+                            object.set_selected(!object.is_selected());
+                        }
+                    }
+                    else {
+                        object.set_selected(is_under_mouse);
+                    }
+                }
             }
         }
     }
