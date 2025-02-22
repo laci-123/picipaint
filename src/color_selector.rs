ecb133157de7f2ca10f0808b7f0047a4695719d6
Gohér László <goher.laci@gmail.com>
2025-02-22 18:00:51 +0100

    color selector window can be closed by Esc or by clicking somewher else

--- src/color_selector.rs
+++ src/color_selector.rs
@@ -10,25 +10,31 @@ pub struct ColorSelector {
 
 impl ColorSelector {
     pub fn update(&mut self, ctx: &egui::Context, color: &mut egui::Color32) {
+        ctx.input(|input| {
+            if input.key_down(egui::Key::Escape) {
+                self.is_open = false;
+            }
+        });
+
         if self.is_open {
             // Goal: open the window centered but allow moving it. But...
             // 
             // window.default_pos(...) doesn't work for some reason,
             // and window.anchor(...) disables moving the window,
             // so we have to do it like this.
-            // It also isn't enough to just do window.anchor(...) on only the
+            // It also doesn't work to just do window.anchor(...) on only the
             // first frame after the window's been opened.
             // We have to actually wait for a little while.
 
             let opening_time = self.opening_time.get_or_insert(Instant::now());
             
-            let mut window = egui::Window::new("Foreground color").collapsible(false);
+            let mut window = egui::Window::new("Foreground color").collapsible(false).resizable(false);
 
             if Instant::now().duration_since(*opening_time) < Duration::from_millis(100) {
                 window = window.anchor(egui::Align2::CENTER_CENTER, egui::Vec2::ZERO);
             }
 
-            window.show(ctx, |ui| {
+            let response = window.show(ctx, |ui| {
                 // If Color32's r, g, b and a fields were public then it would be much easier to do this.
                 let mut red = color.r();
                 let mut green = color.g();
@@ -39,6 +45,12 @@ impl ColorSelector {
                 ui.add(egui::Slider::new(&mut blue, 0..=255).text("blue"));
                 *color = egui::Color32::from_rgba_premultiplied(red, green, blue, alpha);
             });
+
+            if let Some(r) = response {
+                if r.response.clicked_elsewhere() && Instant::now().duration_since(*opening_time) > Duration::from_millis(100) {
+                    self.is_open = false;
+                }
+            }
         }
         else {
             self.opening_time = None;
