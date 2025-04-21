621a62a8702a6216215280ea607adfd04bc5fa51
Gohér László <goher.laci@gmail.com>
2025-04-21 15:01:06 +0200

    refactor egui_painter.rs

--- src/egui_painter.rs
+++ src/egui_painter.rs
@@ -1,7 +1,6 @@
 use eframe::egui;
-use crate::primitives;
+use crate::primitives::*;
 use crate::engine;
-use crate::primitives::ScreenSpace;
 
 
 pub struct EguiPainter {
@@ -13,20 +12,20 @@ pub struct EguiPainter {
 impl engine::ScreenPainter for EguiPainter {
     type Texture = egui::TextureHandle;
     
-    fn draw_line(&mut self, start: primitives::Vector2<ScreenSpace>, end: primitives::Vector2<ScreenSpace>, stroke: primitives::Stroke) {
+    fn draw_line(&mut self, start: Vector2<ScreenSpace>, end: Vector2<ScreenSpace>, stroke: Stroke) {
         self.painter.line_segment([egui::Pos2::from(start), egui::Pos2::from(end)], egui::Stroke::from(stroke));
     }
     
-    fn draw_circle(&mut self, center: primitives::Vector2<ScreenSpace>, radius: f32, stroke: primitives::Stroke) {
+    fn draw_circle(&mut self, center: Vector2<ScreenSpace>, radius: f32, stroke: Stroke) {
         self.painter.circle_filled(egui::Pos2::from(center), radius, stroke.color);
     }
     
-    fn draw_rectangle(&mut self, rectangle: primitives::Rectangle<ScreenSpace>, stroke: primitives::Stroke) {
+    fn draw_rectangle(&mut self, rectangle: Rectangle<ScreenSpace>, stroke: Stroke) {
         let corner_rounding = 0.0;
         self.painter.rect_stroke(egui::Rect::from(rectangle), corner_rounding, stroke);
     }
     
-    fn draw_rectangle_filled(&mut self, rectangle: primitives::Rectangle<ScreenSpace>, color: primitives::Color, stroke: Option<primitives::Stroke>) {
+    fn draw_rectangle_filled(&mut self, rectangle: Rectangle<ScreenSpace>, color: Color, stroke: Option<Stroke>) {
         let rect = egui::Rect::from(rectangle);
         let corner_rounding = 0.0;
         if let Some(s) = stroke {
@@ -45,7 +44,7 @@ impl engine::ScreenPainter for EguiPainter {
         self.context.load_texture(name, color_image, egui::TextureOptions::default())
     }
     
-    fn draw_image(&mut self, frame: primitives::Rectangle<ScreenSpace>, texture: &Self::Texture) {
+    fn draw_image(&mut self, frame: Rectangle<ScreenSpace>, texture: &Self::Texture) {
             self.painter.image(texture.id(),
                                egui::Rect::from(frame),
                                egui::Rect::from_min_max(egui::Pos2 { x: 0.0, y: 0.0 }, egui::Pos2 { x: 1.0, y: 1.0 }),
@@ -63,8 +62,8 @@ impl EguiPainter {
 }
 
 
-impl<T: primitives::Tag> From<primitives::Vector2<T>> for egui::Pos2 {
-    fn from(other: primitives::Vector2<T>) -> egui::Pos2 {
+impl<T: Tag> From<Vector2<T>> for egui::Pos2 {
+    fn from(other: Vector2<T>) -> egui::Pos2 {
         egui::Pos2 {
             x: other.x,
             y: other.y,
@@ -73,36 +72,36 @@ impl<T: primitives::Tag> From<primitives::Vector2<T>> for egui::Pos2 {
 }
 
 
-impl<T: primitives::Tag> From<egui::Pos2> for primitives::Vector2<T> {
-    fn from(other: egui::Pos2) -> primitives::Vector2<T> {
-        primitives::Vector2::new(other.x, other.y)
+impl<T: Tag> From<egui::Pos2> for Vector2<T> {
+    fn from(other: egui::Pos2) -> Vector2<T> {
+        Vector2::new(other.x, other.y)
     }
 }
 
 
-impl<T: primitives::Tag> From<egui::Vec2> for primitives::Vector2<T> {
-    fn from(other: egui::Vec2) -> primitives::Vector2<T> {
-        primitives::Vector2::new(other.x, other.y)
+impl<T: Tag> From<egui::Vec2> for Vector2<T> {
+    fn from(other: egui::Vec2) -> Vector2<T> {
+        Vector2::new(other.x, other.y)
     }
 }
 
 
-impl From<primitives::Color> for egui::Color32 {
-    fn from(other: primitives::Color) -> egui::Color32 {
+impl From<Color> for egui::Color32 {
+    fn from(other: Color) -> egui::Color32 {
         egui::Color32::from_rgba_premultiplied(other.red, other.green, other.blue, other.alpha)
     }
 }
 
 
-impl From<primitives::Stroke> for egui::Stroke {
-    fn from(other: primitives::Stroke) -> egui::Stroke {
+impl From<Stroke> for egui::Stroke {
+    fn from(other: Stroke) -> egui::Stroke {
         egui::Stroke::new(other.thickness.value, other.color)
     }
 }
 
 
-impl<T: primitives::Tag> From<primitives::Rectangle<T>> for egui::Rect {
-    fn from(other: primitives::Rectangle<T>) -> egui::Rect {
+impl<T: Tag> From<Rectangle<T>> for egui::Rect {
+    fn from(other: Rectangle<T>) -> egui::Rect {
         egui::Rect::from_min_max(egui::Pos2::from(other.p1), egui::Pos2::from(other.p2))
     }
 }
