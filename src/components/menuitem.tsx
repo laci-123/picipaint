d0054b11420defabc46dcc66bebd1bc389f00cac
Gohér László <goher.laci@gmail.com>
2026-03-05 15:53:09 +0100

    floating submenus that can be opend/closed by clicking on menu items

--- src/components/menuitem.tsx
+++ src/components/menuitem.tsx
@@ -1,13 +1,37 @@
+import { useEffect, useRef, useState, type RefObject } from "react";
+
 interface Props {
   caption: string;
   children?: React.ReactNode;
 }
 
 export default function MenuItem(props: Props) {
-  return <div className="menu-item">
-           <div className="menu-item-head" title={props.caption}>{props.caption}</div>
+  const [bodyVisible, setBodyVisible] = useState(false);
+  const ref: RefObject<HTMLDivElement | null> = useRef(null);
+
+  function toggleVisibility() {
+    setBodyVisible(!bodyVisible);
+  }
+
+  const handleClickOutside = (event: PointerEvent) => {
+      if (ref.current && !(event.target instanceof Node && ref.current.contains(event.target))) {
+          setBodyVisible(false);
+      }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  }, []);
+
+  return <div className="menu-item" ref={ref}>
+           <div className="menu-item-head" onClick={toggleVisibility} title={props.caption}>
+             {props.caption}
+           </div>
            {
-            props.children != undefined ?
+            props.children != undefined && bodyVisible ?
             <div className="menu-item-body">{props.children}</div> :
             null
            }
