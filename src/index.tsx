aa004399b96e8c57d521c578a58c26c16130dc69
Gohér László <goher.laci@gmail.com>
2026-03-05 18:06:06 +0100

    rename react root element in index.html from "app" to "react-root"

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,5 @@ import { createRoot } from "react-dom/client"
 
 
 
-const root = createRoot(document.getElementById("app")!);
+const root = createRoot(document.getElementById("react-root")!);
 root.render(<App />);
